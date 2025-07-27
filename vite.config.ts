75d433685c65b8d108fbadaf4f2564df14b41dac
Yoshimitzu Calderon <yoshimitzu.calderon@gmail.com>
2025-07-27 03:28:39 -0700

    Rev 2: App corriendo (Atendiendo errores Vercel)

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+// Comentado temporalmente debido a errores de compatibilidad
+// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
+    // Comentado temporalmente debido a errores de compatibilidad
+    // runtimeErrorOverlay(),
+    // ...(process.env.NODE_ENV !== "production" &&
+    // process.env.REPL_ID !== undefined
+    //   ? [
+    //       await import("@replit/vite-plugin-cartographer").then((m) =>
+    //         m.cartographer(),
+    //       ),
+    //     ]
+    //   : []),
   ],
   resolve: {
     alias: {
